39ab23ed5d7cbf14de0978ce0a8d7714a3dd3c1d
Andy Myers <mail@jamyers.net>
2019-03-03 11:28:42 -0500

    Added code to store notes to DB

--- app/src/main/java/local/andy/jamyers/plainolnotes/EditorActivity.java
+++ app/src/main/java/local/andy/jamyers/plainolnotes/EditorActivity.java
@@ -1,13 +1,21 @@
 package local.andy.jamyers.plainolnotes;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private String action;
+    private EditText editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +24,66 @@ public class EditorActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editor = findViewById(R.id.editText);
+
+        Intent intent = getIntent()
+;
+        Uri uri = ((Intent) intent).getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
+        if(uri == null) {
+            action = Intent.ACTION_INSERT;
+            setTitle(R.string.new_note);
+        }
+
         //Todo :  Find out how to get back button on title bar
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finishEditing();
+                break;
+        }
+
+
+        return true;
+    }
+
+    private void finishEditing() {
+        String new_text = editor.getText().toString().trim();
+
+        switch(action){
+            case Intent.ACTION_INSERT:
+                if(new_text.length() == 0){
+                    setResult(RESULT_CANCELED);
+                } else {
+                    insertNote(new_text);
+                }
+        }
+        finish();
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    @Override
+    public void onBackPressed(){
+        finishEditing();
     }
 }
--- app/src/main/java/local/andy/jamyers/plainolnotes/MainActivity.java
+++ app/src/main/java/local/andy/jamyers/plainolnotes/MainActivity.java
@@ -136,4 +136,11 @@ implements LoaderManager.LoaderCallbacks<Cursor>
         Intent intent = new Intent(this, EditorActivity.class);
         startActivityForResult(intent, EDITOR_REQUEST_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
+            restartLoader();
+        }
+    }
 }
